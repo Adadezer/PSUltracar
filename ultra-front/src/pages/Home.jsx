49feb328eb29cfbcb88f014c6f64695081a171b2
Adadezer <adadezer@gmail.com>
2023-04-18 00:30:54 -0300

    refactor(frontend): :recycle: adição do botão 'consultar' para ir para a proxima tela

--- ultra-front/src/pages/Home.jsx
+++ ultra-front/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import QRCode from 'react-qr-code';
 import { useContext } from 'react';
-import { Grid, TextField } from '@mui/material';
+import { Grid, TextField, Stack, Button } from '@mui/material';
 import '../App.css';
 import UltracarContext from '../context/UltracarContext';
-import ScreenService from '../components/ScreenService';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const { link, setLink } = useContext(UltracarContext);
+  const navigate = useNavigate();
+  const Consult = () => navigate('/client');
 
   function handleQRCode(e) {
     setLink(`${e.target.value}`);
@@ -14,24 +16,26 @@ function Home() {
   return (
     <Grid
       container
-      justifyContent="center"
-      alignSelf="center"
+      justifyContent='center'
+      alignSelf='center'
     >
       <Grid item >
-        <div className="container-item">
+        <div className='container-item'>
           <TextField
             className='input'
-            helperText="digite o nome do cliente"
-            label="cliente"
-            variant="outlined"
+            helperText='digite o nome do cliente'
+            label='cliente'
+            variant='outlined'
             value={ `${link}` }
             onChange={(e) => handleQRCode(e)}
           />
-          <ScreenService />
-          <div>
+          <Stack spacing={2} direction='row'>
+            <Button variant='outlined' onClick={Consult}>Consultar</Button>
+          </Stack>
+          <Grid item>
             <p>Ou acesse o QrCode</p>
             <QRCode value={`http://localhost:3000/client`}/>
-          </div>
+          </Grid>
         </div>
       </Grid>  
     </Grid>
