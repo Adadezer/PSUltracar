e4c0863b90114f1fc3be380723d796eade2ba8e4
Adadezer <adadezer@gmail.com>
2023-04-18 16:29:35 -0300

    feat(frontend): :sparkles: link no navbar para voltar a tela inicial

--- ultra-front/src/components/NavBar.jsx
+++ ultra-front/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom';
 
 const StyledAvatar = styled(Avatar)(({ theme }) => ({
   width: theme.spacing(7),
@@ -12,15 +13,17 @@ const StyledAvatar = styled(Avatar)(({ theme }) => ({
 
 const NavBar = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: 'rgb(105, 156, 214)'}}>
+    <AppBar position='static' sx={{ backgroundColor: 'rgb(105, 156, 214)'}}>
       <Toolbar>
-        <StyledAvatar alt="Avatar" src="/cabecalho.png" />
+        <Link to={'/'}>
+          <StyledAvatar alt='Avatar' src='/cabecalho.png' />
+        </Link>
         <Typography
-          variant="h5"
-          component="div"
+          variant='h5'
+          component='div'
           sx={{ flexGrow: 1, marginLeft: '10px', fontWeight: '500'}}
         >
-          Ultracar
+          <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Ultracar</Link>
         </Typography>
       </Toolbar>
     </AppBar>
